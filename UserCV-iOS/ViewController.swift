44fa2f9b7e32a69fc31f5e3866e3474248216192
eee <eee>
2019-12-30 10:30:36 +0000

    Initial Commit

---
+++ UserCV-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserCV-iOS
+//
+//  Created by alumno on 30/12/2019.
+//  Copyright © 2019 miguelleonmarti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
